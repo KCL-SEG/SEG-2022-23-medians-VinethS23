52efcb9a171ae8d6c2121eeaae30b8ace6cb1ea3
Vineth Siriwardana <VASiriwardana@gmail.com>
2023-10-03 00:07:06 +0100

    Created the median calculation function

--- median.py
+++ median.py
@@ -1,12 +1,23 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+#integer division - x//y = x divided by y, the answer is rounded DOWN to the nearest int
 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        itemCount = len(numbers) # not numbers.len()
+        numbers.sort()
+        if (itemCount % 2) == 1:
+            median = numbers[itemCount//2]
+        elif (itemCount % 2) == 0:
+            x = itemCount//2
+            y = (itemCount//2)+1
+            median = (numbers[x] + numbers[y])//2
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
+
+print("Median: ", median)
 print(numbers)
